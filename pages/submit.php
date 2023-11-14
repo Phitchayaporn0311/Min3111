8818525a8553f14b68f5284e1fc6ad8bd061f860
Phitchayaporn0311 <min2210.we@gmail.com>
2023-11-14 11:16:54 +0700

    submit.php

---
+++ pages/submit.php
@@ -0,0 +1,23 @@
+<?php
+    $email = $_POST['email'];
+    $password = $_POST['passeord'];
+    $city = $_POST['city'];
+    $web = $_POST['web'];
+    $role = $_POST['role'];
+    $sign1 = $_POST['sign1'];
+    $sign2 = $_POST['sign2'];
+    $sign3 = $POST['sign3'];
+
+
+    echo 'email: '.$email.'<br>';
+    echo 'password: '.$passwoed.'<br>';
+    echo 'city: '.$city.'<br>';
+    echo 'web: '.$web.'<br>';
+    echo 'role: '.$role.'<br>';
+    echo 'sign1: '.$sign1.'<br>';
+    echo 'sign2: '.$sign2.'<br>';
+    echo 'sign3: '.$sign3.'<br>';
+    
+
+
+?>      
